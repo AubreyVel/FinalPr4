cda333c1930363ad451aad27e1363f46f0b75568
AubreyVel <agvelasco@amarillocollege.com>
2025-05-07 14:27:37 -0500

    update

--- FinalPr4/Program.cs
+++ FinalPr4/Program.cs
@@ -10,7 +10,55 @@ namespace FinalPr4
     {
         static void Main(string[] args)
         {
+            int[] Array = InputArray();
+            DisplayArray(Array); //printarray, just accidently name soemthin else
+            FindMaximum(Array);
 
+
+            Console.ReadLine();
         }
+
+        static int[] InputArray()
+        {
+            int count = 0;
+            int[] inputArray = new int[10];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+
+                Console.Write("Enter a integer for number " + count + " ");
+                count++;
+                inputArray[i] = Convert.ToInt32(Console.ReadLine());
+                //inputArray[i];
+            }
+            return inputArray;
+        }
+
+
+        static void DisplayArray(int[] inputArray)
+        {
+            for (int i = 0;i< inputArray.Length ; i++)
+            {
+                Console.Write(inputArray[i] +" ");
+            }
+            Console.ReadLine();
+        }
+
+        static void FindMaximum(int[] inputArray)
+        {
+            int max = 0;
+            for (int i = 0; i<inputArray.Length; i++)
+            {
+                if (inputArray[i] > max)
+                {
+                    max = inputArray[i];
+                }
+            }
+            Console.WriteLine(max);
+        }
+
+
+
+
     }
 }
